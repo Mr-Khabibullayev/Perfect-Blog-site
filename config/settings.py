4dc27c5aed39f041a5036d63e68630ec38e1bd22
Mr-Khabibullayev <habibullayevaxrorbek960@gmail.com>
2022-12-31 19:08:57 +0500

    setted anythings

--- config/settings.py
+++ config/settings.py
@@ -134,7 +134,7 @@ STATICFILES_STOAGE = 'whitenoise.storage.CompressMainfestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField' 
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
